40eacc02c130015021856c0325b7ff41535ee595
Joao Peres <joaovitorperes06@gmail.com>
2026-09-11 05:49:02 -0300

    UPDT test12

--- teste12/script.js
+++ teste12/script.js
@@ -29,3 +29,53 @@ const lista = document.getElementById("listaSoldados")
       lista.appendChild(item);
    });
 
+   // TESTE 01
+// textContent: troca o conteudo do texto selecionado por outro
+const test1 = document.querySelector(".t1");
+test1.textContent = "TESTE 01";
+
+   // TESTE 02
+// style.color: troca a cor do elemento selecionado por outro
+const test2 = document.querySelector(".t2");
+test2.style.color = "red";
+
+
+   // TESTE 03/04
+// querySelectorAll: seleciona todos os elementos da mesma tag, se a alteraçao for style, é necessario utilizar um forEach
+
+//querryselector: seleciona apenas o primeiro elemento da tag encontrada
+const test3 = document.querySelectorAll(".t3");
+test3.forEach((elemento) => {
+   elemento.style.color = "orange";
+});
+
+   // TESTE 05
+// addEventListener: 'escuta' até que o comando especificado seja acionado, nesse caso 'click', quando o evento acontece, executa algum comando
+const botao = document.querySelector(".botao"); 
+//botao
+
+const texto = document.querySelector(".texto");
+//texto
+
+botao.addEventListener("click", () => {
+   texto.textContent = "TESTE05";
+})
+
+
+   // TESTE 06
+
+   let cont = 0;
+
+const botaoMais = document.querySelector(".mais");
+const botaoMenos = document.querySelector(".menos");
+const contador = document.querySelector(".cont")
+
+botaoMais.addEventListener("click", () => {
+   cont++;
+   contador.textContent = cont;
+})
+
+botaoMenos.addEventListener("click", () => {
+   cont--;
+   contador.textContent = cont;
+})
